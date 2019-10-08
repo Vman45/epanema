d81e18c7ee806494a6b4b88e2c683a716e23f777
Philippe Jean Guillaumie <similar@orange.fr>
2019-10-08 08:10:38 +0200

    Remove libcurl4-gnutls-dev and libgnutls28-dev from dependencies

--- epanema.sh
+++ epanema.sh
@@ -66,12 +66,11 @@ DEPS="aspell automake build-essential ccache check cmake cowsay doxygen \
 faenza-icon-theme git gstreamer1.0-libav gstreamer1.0-plugins-bad \
 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly imagemagick \
 libasound2-dev libavahi-client-dev libblkid-dev libbluetooth-dev \
-libbullet-dev libcogl-gles2-dev libcurl4-gnutls-dev libexif-dev \
-libfontconfig1-dev libfreetype6-dev libfribidi-dev libgeoclue-2-dev \
-libgif-dev libgnutls28-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
-libharfbuzz-dev libibus-1.0-dev libinput-dev libjpeg-dev libluajit-5.1-dev \
-liblz4-dev libmount-dev libopenjp2-7-dev libosmesa6-dev libpam0g-dev \
-libpoppler-cpp-dev libpoppler-dev libpoppler-private-dev \
+libbullet-dev libcogl-gles2-dev libexif-dev libfontconfig1-dev libfreetype6-dev \
+libfribidi-dev libgeoclue-2-dev libgif-dev libgstreamer1.0-dev \
+libgstreamer-plugins-base1.0-dev libharfbuzz-dev libibus-1.0-dev libinput-dev \
+libjpeg-dev libluajit-5.1-dev liblz4-dev libmount-dev libopenjp2-7-dev \
+libosmesa6-dev libpam0g-dev libpoppler-cpp-dev libpoppler-dev libpoppler-private-dev \
 libpulse-dev libraw-dev librsvg2-dev libscim-dev libsndfile1-dev \
 libspectre-dev libssl-dev libsystemd-dev libtiff5-dev libtool \
 libudev-dev libudisks2-dev libunibreak-dev libunwind-dev \
